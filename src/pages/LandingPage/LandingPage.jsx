ea329afe6731fdb7b73cdb4b8f9d1c194008a2c0
justintea <justin.tehhy@gmail.com>
2024-02-08 01:25:27 +0800

    feat: updated landingpage carousel images and imgsrc

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,38 @@
-// import NavbarIn from "../../components/Navbar/NavbarIn/NavbarIn"
 import NavbarOut from "../../components/Navbar/NavbarOut/NavbarOut"
-import AuthPage from "../AuthPage/AuthPage"
+import React from 'react';
+import { Carousel } from 'antd';
+
+const contentStyle = {
+  height: '100%',
+  color: '#fff',
+  lineHeight: '1750%',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
 export default function LandingPage() {
   return (
     <>
-      {/* <NavbarIn /> */}
-      {/* //? J 24/1 0050: comment out in-Navbar, as this is prelogin env */}
-  
       <NavbarOut />
-      <h1>Landing Page</h1>
+      {/* <h1>Landing Page</h1> */}
+      <Carousel autoplay>
+    <div>
+          <img src="../../../screenshots/carousel1.jpg" alt="carousel1" style={contentStyle} />
+          {/* <h3 style={contentStyle}>1</h3> */}
+    </div>
+        <div>
+          <img src="../../../screenshots/carousel2.jpg" alt="carousel2" style={contentStyle} />
+      {/* <h3 style={contentStyle}>2</h3> */}
+    </div>
+        <div>
+        <img src="../../../screenshots/carousel3.jpg" alt="carousel3" style={contentStyle} />
+      {/* <h3 style={contentStyle}>3</h3> */}
+    </div>
+        <div>
+        <img src="../../../screenshots/carousel4.jpg" alt="carousel4" style={contentStyle} />
+      {/* <h3 style={contentStyle}>4</h3> */}
+    </div>
+  </Carousel>
       
     </>
   )
