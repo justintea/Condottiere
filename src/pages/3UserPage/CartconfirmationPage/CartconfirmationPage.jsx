f09f372d4474420a2dbc083a0f77413bcd33f2c2
justintea <justin.tehhy@gmail.com>
2024-02-12 01:16:29 +0800

    feat: difficult, slow progress on items in a Shopping Cart. created Delete button per item, Checkout button for the Order

--- src/pages/3UserPage/CartconfirmationPage/CartconfirmationPage.jsx
+++ src/pages/3UserPage/CartconfirmationPage/CartconfirmationPage.jsx
@@ -1,5 +1,14 @@
 import { Divider } from "antd";
 import { Avatar, List } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
 
 export default function CartconfirmationPage({ user }) {
   
@@ -31,62 +40,179 @@ function isObjectWithNull(obj) {
   );
 }
 
+const onFinish = (valuesConfirmed) => {
+  console.log('Success:', valuesConfirmed);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+  
+    
   return (<>
     <Divider orientation="left" style={{margin: "0px" }}> <h2 style={{ fontFamily: 'Palatino Linotype'}}>Shopping Cart</h2> </Divider>
 
-    {/* //? tester code - can render your localStorage prelogin form data  
-    <p> to be rendered nicely</p>
+     {/* //? tester code - can render your localStorage prelogin form data   */}
+     <p> to be rendered nicely</p>
     <div>
     parsedData = {JSON.stringify(parsedData, null, 2)}
-    </div> */}
+    </div>
+
+    <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+    >
+      
+      <List
+       
+       itemLayout="horizontal"
+       dataSource={transformedData}
+       renderItem={(item, index) => (
+         <List.Item>
+           <List.Item.Meta
+             avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+             
+             // * Renders the service title and details properly
+             title={<h2>{item.title}</h2>}
+             description={JSON.stringify(item.details)}
+   
+           />
+   
+           <Button> Delete </Button>
+         </List.Item>
+       )}
+     />
+   
+
+
+    <Form.Item
+      wrapperCol={{
+        offset: 25,
+        span: 16,
+        }}
+        style={{ display: 'flex', justifyContent: 'flex-end' }}
+    >
+      <Button style={{ backgroundColor: "#01628f", width: '180px'}} type="primary" htmlType="submit"> Proceed to Checkout </Button>
+    </Form.Item>
+  </Form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
     
-    <List
-    itemLayout="horizontal"
-    dataSource={transformedData}
-    renderItem={(item, index) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-          
-          // * Renders the service title and details properly
-          title={<h2>{item.title}</h2>}
-          description={JSON.stringify(item.details)}
-
-        />
-      </List.Item>
-    )}
-  />
-
-    
-      {/* //? -----------------------------------------------------------
-
-    <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item, index) => (
-      <List.Item>
-        <List.Item.Meta
-          img 
-          title={<a href="https://ant.design">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  /> 
-
-//? ----------------------------------------------------------- */}
-
+   
   </>);
 }
-// return (<>
-//   <h2>Shopping Cart </h2>
-//   <p> to be rendered nicely</p>
-//   {data1PaintSvcs.map((item, index) => (
-//       <div key={index}>
-//         {/* Render the properties of each item as needed */}
-//         <p>{item.name}</p>
-//         <p>{item.qty}</p>
-//         {/* Add more properties or customize the rendering based on your data structure */}
-//       </div>
-//     ))}
-// </>);
+
+
+
+
+
+
+
+
+
+
+
+//? 11/2 savept
+// export default function CartconfirmationPage({ user }) {
+  
+//   const localStorageData = localStorage.getItem('data1Key');
+//   const parsedData = JSON.parse(localStorageData);
+
+//   //* need to transform 
+//   const transformedData = [];
+//   //* Function to transform my ParsedData into a form the initial AntD List would take 
+//   for (const [key, value] of Object.entries(parsedData)) {
+//       if (!isObjectWithNull(value)) {
+//         transformedData.push({
+//           title: key,
+//           details: value
+//         });
+//     }
+//   }
+  
+//   console.log(transformedData);
+
+// //* Helper function to check if the embedded value is null 
+// function isObjectWithNull(obj) {
+//   return (
+//     typeof obj === 'object' &&
+//     obj !== null &&
+//     Object.keys(obj).length === 1 &&
+//     obj.hasOwnProperty('null') &&
+//     obj.null === null
+//   );
+// }
+
+//   return (<>
+//     <Divider orientation="left" style={{margin: "0px" }}> <h2 style={{ fontFamily: 'Palatino Linotype'}}>Shopping Cart</h2> </Divider>
+
+//     {/* //? tester code - can render your localStorage prelogin form data   */}
+//     <p> to be rendered nicely</p>
+//     <div>
+//     parsedData = {JSON.stringify(parsedData, null, 2)}
+//     </div>
+    
+//     <List
+       
+//     itemLayout="horizontal"
+//     dataSource={transformedData}
+//     renderItem={(item, index) => (
+//       <List.Item>
+//         <List.Item.Meta
+//           avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+          
+//           // * Renders the service title and details properly
+//           title={<h2>{item.title}</h2>}
+//           description={JSON.stringify(item.details)}
+
+//         />
+//       </List.Item>
+//     )}
+//   />
+
+//   </>);
+// }
