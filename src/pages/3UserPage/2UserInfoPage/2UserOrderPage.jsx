4f6269823788795739199b1619488d9145720ac7
justintea <justin.tehhy@gmail.com>
2024-02-14 15:44:53 +0800

    feat: WIP - on rendering Orders state

--- src/pages/3UserPage/2UserInfoPage/2UserOrderPage.jsx
+++ src/pages/3UserPage/2UserInfoPage/2UserOrderPage.jsx
@@ -8,83 +8,11 @@ import { getToken } from "../../../utilities/0usersService";
 export default function UserOrderPage() {
   const { orders, setOrders } = useOutletContext();
   const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState([]);
   const [data, setData] = useState([]);
-  //? try 3. 
-  const [hasMore, setHasMore] = useState(true);
 
-  //? probably a list 
-  console.log(orders);
-  //? many arrays
-  //? for arrays, go into object 
-
-  const loadMoreData = () => {
-    if (loading) {
-      return;
-    }
-    setLoading(true);
-    fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
-      .then((res) => res.json())
-      .then((body) => {
-        setData([...data, ...body.results]);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  };
-  useEffect(() => {
-    loadMoreData();
-  }, []);
+  console.log('this is the Orders state :', orders);
   
-
-   //? try 2. use Order state method
-  //  if (loading) {
-  //     return;
-  //   }
-  //  setLoading(true);
-   
-  //  fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
-  //   fetch('/api/orders', options)
-
-  //     .then((res) => res.json())
-  //     .then((body) => {
-  //       setData([...data, ...body.results]);
-  //       setLoading(false);
-  //   })
-   
-  // setData(...orders, orders);
-  //      setLoading(false);
-      
-   
-   //? try 1. fetch GET method
-//    if (loading) {
-//     return;
-//   }
-//  setLoading(true);
- 
-//  const token = getToken();
-
-//  const headers = {
-//   "Content-type": "application/json",
-//   Authorization: `Bearer ${token}`,
-// };
-
-//  const options = {
-//   method: "GET",
-//   headers,
-// };
-// //  fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
-//  await fetch('/api/orders', options)
-
-//     .then((res) => res.json())
-//     .then((body) => {
-//       setData([...data, ...body.results]);
-//       setLoading(false);
-//     })
-//     .catch(() => {
-//       setLoading(false);
-//     });
-
    //? savept
 //    if (loading) {
 //     return;
@@ -121,12 +49,41 @@ export default function UserOrderPage() {
   //   loadMoreData();
   // }, []);
 
+  const renderSubDetails = (details) => {
+    return Object.entries(details).map(([key, value]) => (
+      <p key={key}>
+        <strong>{key}:</strong> {value}
+      </p>
+    ));
+  };
+
+  
   
   return (
     <>
       <h2 style={{ fontFamily: "Palatino Linotype" }}> Your Orders </h2>
       <p> probably a list 'Orders', mapped into items and rendered here</p>
-      <div
+      
+      {/* //? start of experiment 2 */}
+      <ul>
+      {orders.map((order) => (
+        <li key={order._id}>
+          <h3>{order.items[0].title}</h3>
+          {renderSubDetails(order.items[0].details)}
+
+          {/* <p>
+            <strong>Details:</strong> {order.items[0].details.numberOfModels} models,{' '}
+            {order.items[0].details.sizeComplexity} size, {order.items[0].details.urgencyRequired} urgency
+          </p> */}
+          <p>
+            <strong>Item Price:</strong> {order.items[0].details.itemPrice}
+          </p>
+        </li>
+      ))}
+    </ul>
+
+      {/* //? start of experiment 1 */}
+      {/* <div
       id="scrollableDiv"
       style={{
         height: 400,
@@ -153,32 +110,79 @@ export default function UserOrderPage() {
       >
         <List
           dataSource={data}
-          renderItem={(orders) => (
-            <List.Item key={orders._id}>
+          renderItem={(item) => (
+            <List.Item key={item.email}>
               <List.Item.Meta
-                // avatar={<Avatar src={item.picture.large} />}
-                //  this should be the Order number
-                title={<span>{orders._id}</span>}
-                // this should be details of the order 
-                description={`Date: ${new Date(orders.dateTime).toLocaleString()}`}       
+                avatar={<Avatar src={item.picture.large} />}
+                title={<a href="https://ant.design">{item.name.last}</a>}
+                description={item.email}
               />
-                {orders.items.map((item, index) => (
-          <div key={index}>
-            <strong>Service:</strong> {item.title} <br />
-            <strong>Details:</strong> {JSON.stringify(item.details)} <br />
-            <strong>Total Price:</strong> {item.details.itemPrice} <br />
-            <br />
-          </div>
-        ))}
-              {/* this should be the total price */}
-              <div>Content</div>              
+              <div>Content</div>
             </List.Item>
           )}
         />
       </InfiniteScroll>
-      </div>
+    </div> */}
       <br></br>
       <br></br>
     
     </>);
 }
+
+
+
+
+//? interesting potential
+// {/* <div
+//       id="scrollableDiv"
+//       style={{
+//         height: 400,
+//         overflow: 'auto',
+//         padding: '0 16px',
+//         border: '1px solid rgba(140, 140, 140, 0.35)',
+//       }}
+//     >
+//       <InfiniteScroll
+//         dataLength={data.length}
+//         next={loadMoreData}
+//         hasMore={data.length < 50}
+//         loader={
+//           <Skeleton
+//             avatar
+//             paragraph={{
+//               rows: 1,
+//             }}
+//             active
+//           />
+//         }
+//         endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+//         scrollableTarget="scrollableDiv"
+//       >
+//         <List
+//           dataSource={data}
+//           renderItem={(orders) => (
+//             <List.Item key={orders._id}>
+//               <List.Item.Meta
+//                 // avatar={<Avatar src={item.picture.large} />}
+//                 //  this should be the Order number
+//                 title={<span>{orders._id}</span>}
+//                 // this should be details of the order 
+//                 description={`Date: ${new Date(orders.dateTime).toLocaleString()}`}       
+//               />
+//                 {orders.items.map((item, index) => (
+//           <div key={index}>
+//             <strong>Service:</strong> {item.title} <br />
+//             <strong>Details:</strong> {JSON.stringify(item.details)} <br />
+//             <strong>Total Price:</strong> {item.details.itemPrice} <br />
+//             <br />
+//           </div>
+//         ))}
+//               {/* this should be the total price */}
+//               <div>Content</div>              
+//             </List.Item>
+//           )}
+//         />
+//       </InfiniteScroll>
+//       </div>
+//       <br></br>
+//       <br></br> */}
